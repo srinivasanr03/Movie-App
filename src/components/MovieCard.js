55523daa173d5263ef4a2d98f35bec1d90c7d78f
Srinivasan <srinivasanrcp39@gmail.com>
2024-12-04 16:27:55 +0530

    added all the necessary files

---
+++ src/components/MovieCard.js
@@ -0,0 +1,11 @@
+import {Component} from 'react'
+
+class MovieCard extends Component{
+    render(){
+        return(
+            <h1>Movie-Card</h1>
+        );
+    }
+}
+
+export default MovieCard;
